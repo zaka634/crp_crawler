7b0b4b01e99df7ad8fbf1c5bd637218650813b1b
zaka634 <zaka634@gmail.com>
2024-03-11 21:44:44 -0400

    Update open_links.js

--- open_links.js
+++ open_links.js
@@ -1,3 +1,16 @@
 function main() {
 	var items = document.querySelectorAll("a.underline.text-blue-400").forEach((item) => window.open(item.href));
 }
+
+
+// build and open links.
+case_numbers = [
+    "MSC2490059742", "MSC2490059745"
+]
+function openBuildLink(caseNumber) {
+    window.open(`https://www.casestatusext.com/cases/${caseNumber}`, '_blank');
+}
+
+caseNumbers.forEach(caseNumber => {
+    openBuildLink(caseNumber);
+});
